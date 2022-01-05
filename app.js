e4241200db2b7822cd1527655e25b9ba63ccb76c
PraffulDhasmana <praffuldhasmana@yahoo.com>
2022-01-06 04:23:34 +0530

    imortjavascript

---
+++ app.js
@@ -0,0 +1,13 @@
+
+
+// console.log("script is working from a saperate js file")
+
+// Input
+
+var userName = prompt("Give me username")
+
+
+// Processing
+var welcomeMessage = "this script is working "+userName
+
+alert (welcomeMessage );
