74f87c2e7f26afe5700f37a62f84131abcd4acd9
PraffulDhasmana <praffuldhasmana@yahoo.com>
2022-01-06 05:48:23 +0530

    input

--- app.js
+++ app.js
@@ -2,12 +2,36 @@
 
 // console.log("script is working from a saperate js file")
 
-// Input
+// // Input
 
-var userName = prompt("Give me username")
+// var userName = prompt("Give me username")
 
 
-// Processing
-var welcomeMessage = "this script is working "+userName
+// // Processing
+// var welcomeMessage = "this script is working "+userName
 
-alert (welcomeMessage );
+// alert (welcomeMessage );
+
+// query selector : A way to tell your browser to use any element(#btn-translate)
+//document is api
+
+// addEventListner intract with app listen to events(click is event below)
+
+// clickHandler is call back
+
+var btnTranslate = document.querySelector("#btn-translate")
+var txtInput = document.querySelector("#txt-input");
+
+console.log(txtInput);
+
+
+
+function clickHandler() 
+    {
+        console.log("Clicked");
+        console.log("Input " , txtInput.value);
+        
+    };
+
+    btnTranslate.addEventListener("click", clickHandler)
+
