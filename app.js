b83b1dcc549916578631ce60f9f683f95cd11814
PraffulDhasmana <praffuldhasmana@yahoo.com>
2022-01-07 07:20:19 +0530

    Banana Done

--- app.js
+++ app.js
@@ -1,5 +1,3 @@
-
-
 // console.log("script is working from a saperate js file")
 
 // // Input
@@ -27,19 +25,36 @@
 
 var btnTranslate = document.querySelector("#btn-translate")
 var txtInput = document.querySelector("#txt-input");
-var outPutDiv = document.querySelector("#output");
+var outputDiv = document.querySelector("#output");
 
+var serverURL = "https://lessonfourapi.praffuldhasmana.repl.co/translate/yoda.json"
 
-function clickHandler() 
+// var serverURL = "https://api.funtranslations.com/translate/minion.json"
 
+function getTranslationURL(text) {
+    return serverURL + "?" + "text=" + text
+}
 
+function errorHandler(error) {
+    console.log("Kuch Gadbad H", error)
+    alert("something wrong with server: bbad mein aio")
+}
 
-    {
-        outPutDiv.innerText = "Traslation : " + txtInput.value ;
-        
+function clickHandler() {
+
+    var inputText = txtInput.value; // taking input}
+
+    //calling server for processing
+
+    fetch(getTranslationURL(inputText))
+        .then(response => response.json())
+        .then(json => {
+            var translatedText = json.contents.translated;
+            outputDiv.innerText = translatedText;
+        })
+
+        .catch(errorHandler)
+
     };
 
     btnTranslate.addEventListener("click", clickHandler)
-
-
-
